f0a2d970258358e71a899bf19eeaa70785f81e93
benhillerich <hillerbh@email.uc.edu>
2023-06-02 14:38:32 -0400

    Commit 1

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,7 @@ public class Main {
         totalCost = Math.round(totalCost * 100) / 100.00;
         salesTax = Math.round(salesTax * 100.0) / 100.0;
         System.out.println("The sales tax on your purchase is " + salesTax + " dollars"
-        + " and the price of purchase was " + priceOfPurchase + " so the total is " +
-                totalCost);
+        + " and the price of purchase was " + priceOfPurchase + " dollars so the total is " +
+                totalCost + " dollars.");
     }
 }
