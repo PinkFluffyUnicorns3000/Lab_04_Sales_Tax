6bbb9d4a2d699b7aee7c328842d8afa16f45ebe1
benhillerich <hillerbh@email.uc.edu>
2023-06-01 20:34:31 -0400

    Commit -1

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        double taxRate = .05;
+        double priceOfPurchase = 35.17;
+        double salesTax = priceOfPurchase * taxRate;
+        salesTax = Math.round(salesTax * 100.0) / 100.0;
+        System.out.println("The sales tax on your purchase is " + salesTax + " dollars.");
+    }
+}
